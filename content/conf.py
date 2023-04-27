f009e372ff073234f3c708af69ced3f69b740e45
Richard Darst <rkd@zgib.net>
2023-04-27 11:49:32 +0300

    Add branding

--- content/conf.py
+++ content/conf.py
@@ -49,6 +49,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx_rtd_theme',
     'sphinx_rtd_theme_ext_color_contrast',
+    'sphinx_aaltoscicomp_branding',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
